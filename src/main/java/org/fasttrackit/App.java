4126af91e881a25dfececb036da2dd2511a87d60
Mihai728 <mihaipopescu728@yahoo.com>
2019-09-02 21:02:27 +0300

    added constraint to avoid having auto-vehicles without engines (studied constructors)

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -7,7 +7,11 @@ package org.fasttrackit;
 public class App 
 {
     public static void main( String[] args ) {
-        Car car = new Car();
+
+        Engine engine2 = new Engine ();
+        engine2.manufacturer = "Ferrari";
+        engine2.capacity = 5000;
+        Car car = new Car(engine2);
         car.name = "Ferrari";
         car.fuelLevel = 60;
         car.mileage = 8.5;
@@ -15,28 +19,24 @@ public class App
         car.maxSpeed = 280;
         car.fuelType = "Gasoline";
 
-        Engine engine = new Engine ();
-        engine.manufacturer = "Ferrari";
-        engine.capacity = 5000;
-        car.engine = engine;
+
         System.out.println(car.name);
         System.out.println(car.traveledDistance);
         System.out.println(car.running);
 
         car .accelerate(60, 1);
 
-        Car car2 = new Car();
+        Engine engine1 = new Engine ();
+        engine1.manufacturer = "Bugatty";
+        engine1.capacity = 5000;
+
+        Car car2 = new Car(engine1);
         car2.name = "Bugatty";
         car2.fuelLevel = 100;
         car2.mileage = 10.58;
         car2.doorCount = 3;
         car2.maxSpeed = 300;
         car2.fuelType = "Kerosen";
-
-        Engine engine2 = new Engine ();
-        engine.manufacturer = "Bugatty";
-        engine.capacity = 5000;
-        car2.engine = engine2;
 
         System.out.println("Car2 name: " + car2.name); // concatenation
         System.out.println("Car1 name: " + car.name); // concatenation
--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -2,7 +2,11 @@ package org.fasttrackit;
 
 // inheritance (is-a relationship)
 public class Car extends AutoVehicle {
+
     int doorCount;
 
 
+    public Car(Engine engine) {
+        super(engine);
+    }
 }
