e31f272d64737768cb84c72ba37c08c0b603b48c
Mihai728 <mihaipopescu728@yahoo.com>
2019-09-02 20:03:00 +0300

    studied methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -23,6 +23,8 @@ public class App
         System.out.println(car.traveledDistance);
         System.out.println(car.running);
 
+        car .accelerate(60, 1);
+
         Car car2 = new Car();
         car2.name = "Bugatty";
         car2.fuelLevel = 100;
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -8,4 +8,22 @@ public class Vehicle {
     double fuelLevel;
     double traveledDistance;
     boolean running;
+
+    public double accelerate(double speed, double durationInHours) {
+        System.out.println(name + " is accelerating with " + speed + " km/h " + durationInHours + " h. ");
+
+        double distance = speed * durationInHours;
+        System.out.println("Distance: " + distance);
+
+        //same result as the above statement
+        traveledDistance = traveledDistance + distance;
+        //traveledDistance += distance;
+        double usedFuel = distance * mileage / 100;
+        System.out.println("Used fuel " + usedFuel);
+
+        fuelLevel -= usedFuel;
+
+        System.out.println("Remaining fuel" + fuelLevel);
+        return distance;
+    }
 }
