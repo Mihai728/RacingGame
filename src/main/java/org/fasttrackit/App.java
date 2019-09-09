44e7a49d7192e896cd214e42df1fd088db8259dc
Mihai728 <mihaipopescu728@yahoo.com>
2019-09-09 19:50:40 +0300

    added encapsulation for Vehicle class; studied static variables.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -14,17 +14,17 @@ public class App
         engine2.manufacturer = "Ferrari";
         engine2.capacity = 5000;
         Car car = new Car(engine2);
-        car.name = "Ferrari";
-        car.fuelLevel = 60;
-        car.mileage = 8.5;
+        car.setName("Ferrari");
+        car.setFuelLevel(60);
+        car.setMileage(8.5);
         car.doorCount = 2;
-        car.maxSpeed = 280;
-        car.fuelType = "Gasoline";
+        car.setMaxSpeed(280);
+        car.setFuelType("Gasoline");
 
 
-        System.out.println(car.name);
-        System.out.println(car.traveledDistance);
-        System.out.println(car.running);
+        System.out.println(car.getName());
+        System.out.println(car.getTraveledDistance());
+        System.out.println(car.isRunning());
 
         car .accelerate(60, 1);
 
@@ -33,22 +33,35 @@ public class App
         engine1.capacity = 5000;
 
         Car car2 = new Car(engine1);
-        car2.name = "Bugatty";
-        car2.fuelLevel = 100;
-        car2.mileage = 10.58;
+        car2.setName("Bugatty");
+        car2.setFuelLevel(100);
+        car2.setMileage(10.58);
         car2.doorCount = 3;
-        car2.maxSpeed = 300;
-        car2.fuelType = "Kerosen";
+        car2.setMaxSpeed(300);
+        car2.setFuelType("Kerosen");
 
-        System.out.println("Car2 name: " + car2.name); // concatenation
-        System.out.println("Car1 name: " + car.name); // concatenation
+        System.out.println("Car2 name: " + car2.getName()); // concatenation
+        System.out.println("Car2 name: " + car2.getTraveledDistance()); // concatenation
+        System.out.println("Car2 name: " + car2.isRunning()); // concatenation
 
-        car = car2;
+        // car = car2;
         System.out.println(4 % 2);
         System.out.println(4 % 3);
 
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         System.out.println("Input from user: " + text);
+
+        System.out.println("Static variables example");
+        // we should only call static variables from the class itself (car),
+        // not from a variable containing an object of that class.
+        /*car.totalCount = 1;
+        System.out.println("Total count in car: " + car.totalCount);
+
+        car2.totalCount = 12;
+        System.out.println("Total count in car after setting car2: " + car.totalCount);
+        System.out.println("Total count in car2: " + car2.totalCount);
+        System.out.println("Total count in Car class: " + Car.totalCount);*/
+
     }
 }
